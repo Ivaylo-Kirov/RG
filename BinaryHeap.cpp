eea5e991bc571158655a97efa30062d40085c544
Ivaylo Kirov <ivo.kirov@gmail.com>
2019-05-07 19:33:54 -0400

    Create BinaryHeap.cpp

---
+++ BinaryHeap.cpp
@@ -0,0 +1,91 @@
+#include "pch.h"
+
+// USAGE
+//BinaryHeap bh;
+//
+//bh.insert(42);
+//bh.insert(55);
+//
+//bh.insert(12);
+//
+//bh.pop();
+
+class BinaryHeap {
+public:
+	size_t capacity;
+	int* buffer;
+	size_t size{};
+
+	void insert(int data) {
+		buffer[size] = data;
+		swim(size);
+		++size;
+	}
+
+	void swim(int index) {
+		if (buffer[index] > buffer[parent(index)]) {
+			std::swap(buffer[index], buffer[parent(index)]);
+			swim(parent(index));
+		}
+	}
+
+	int parent(int index) {
+		return (index - 1) / 2;
+	}
+
+	void pop() {
+		std::swap(buffer[0], buffer[size - 1]);
+		buffer[size - 1] = NULL;
+		--size;
+		sink(0);
+	}
+
+	int leftIndex(int index) {
+		return (index * 2) + 1;
+	}
+
+	int rightIndex(int index) {
+		return (index * 2) + 2;
+	}
+
+	void sink(int index) {
+		int biggerChildIndex = buffer[leftIndex(index)] > buffer[rightIndex(index)] ? leftIndex(index) : rightIndex(index);
+		if (buffer[index] < buffer[biggerChildIndex]) {
+			std::swap(buffer[index], buffer[biggerChildIndex]);
+			sink(biggerChildIndex);
+		}
+	}
+
+	BinaryHeap(size_t capacity = 3)
+		: capacity(capacity)
+		, buffer(new int[capacity])
+	{}
+
+	BinaryHeap(const BinaryHeap& copy)
+		: capacity(copy.capacity)
+		, buffer(new int[capacity])
+	{
+		std::copy(copy.buffer, copy.buffer + copy.capacity, buffer);
+	}
+
+	BinaryHeap(BinaryHeap&& copy)
+		: BinaryHeap()
+	{
+		swap(*this, copy);
+	}
+
+	BinaryHeap& operator=(BinaryHeap copy) {
+		swap(*this, copy);
+		return *this;
+	}
+
+	friend void swap(BinaryHeap& first, BinaryHeap& second) {
+		using std::swap;
+		swap(first.capacity, second.capacity);
+		swap(first.buffer, second.buffer);
+	}
+
+	~BinaryHeap() {
+		delete buffer;
+	}
+};
