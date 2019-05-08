4aacf75120aa1ee536e5f4115bb41ec17b73e69d
Ivaylo Kirov <ivo.kirov@gmail.com>
2019-05-07 23:00:01 -0400

    Create QuickSort.cpp

---
+++ QuickSort.cpp
@@ -0,0 +1,30 @@
+#include "pch.h"
+
+int partition(std::vector<int>& nums, int start_index, int end_index) {
+	int pivot = nums[start_index];
+	int s = start_index + 1;
+	int e = end_index;
+
+	while (s < e) {
+		while (s < e && nums[s] < pivot) {
+			s++;
+		}
+		while (s < e && nums[e] > pivot) {
+			e--;
+		}
+		if (s < e) {
+			std::swap(nums[s], nums[e]);
+		}
+	}
+
+	std::swap(nums[start_index], nums[s - 1]);
+	return s - 1;
+}
+
+void quickSort(std::vector<int>& nums, int start_index, int end_index) {
+	if (start_index < end_index) {
+		int partition_element = partition(nums, start_index, end_index);
+		quickSort(nums, start_index, partition_element);
+		quickSort(nums, partition_element + 1, end_index);
+	}
+}
