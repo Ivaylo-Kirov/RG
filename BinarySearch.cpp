0b296bdebb00d566273ad9148e9fd906b65e91b4
Ivaylo Kirov <ivo.kirov@gmail.com>
2019-07-22 12:35:36 -0400

    Update BinarySearch.cpp

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,3 +1,26 @@
+/*
+
+Iterative
+Takes in vector&, startindex, endindex, searchvalue
+Main loop is while start < end
+Check for matches and return true
+Calculate mid.. End + start / 2
+Is the value Left or right?
+If left.. End = mid
+If right.. Start = mid + 1
+Return false if all values were exhausted and it never matched ‘true’
+
+Recursive
+Takes in vector&, startindex, endindex, searchvalue
+Calculate mid.. End + start / 2
+Check for matches and return true
+Exit condition.. End >= start.. If it’s met, return false (value doesn’t exist)
+is the value left or right?
+If left.. recurse(vector, start, mid, searchvalue)
+If right.. recurse(vector, mid + 1, end, searchvalue)
+
+*/
+
 #include "pch.h"
 
 // USAGE
